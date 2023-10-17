efb509a594891ebb98e328e0f25df7e52cf37116
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2023-10-17 16:27:51 +0700

    first upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Bé MyHanh à!",
+  introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "Nhấn tiếp vào đây nè",
+  title: "Phải chăng em đã say đắm anh ngay từ cái nhìn đầu tiên 😙",
+  desc: "",
+  btnYes: "Đúng ròi rất là thích anh lun <33",
+  btnNo: "Không, Anh trai à :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Chủ nhật này a qua đón e xem phim nha công chúa",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.facebook.com/profile.php?id=100044991774359", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
